71c59e388353f50dfb4be09394e28347f23e97f5
Seth C Whiting <sethcwhiting@gmail.com>
2019-12-23 06:00:35 -0500

    some theming

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header style={{ marginBottom: `1.45rem` }}>
     <div
       style={{
         margin: `0 auto`,
@@ -17,13 +12,7 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link to="/">
           {siteTitle}
         </Link>
       </h1>
