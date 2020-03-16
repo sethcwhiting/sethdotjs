ee03f0882d32ff320e04160929dcf3435f259ec8
Seth C Whiting <sethcwhiting@gmail.com>
2020-03-16 10:18:18 -0400

    makes the chart default to US on page load

--- gatsby/src/pages/corona.js
+++ gatsby/src/pages/corona.js
@@ -30,15 +30,39 @@ const SecondPage = () => {
     const [chartWidth, setChartWidth] = useState(320);
     const [loading, setLoading] = useState(0);
     const [covidData, setCovidData] = useState([]);
-    const [primaryCountry, setPrimaryCountry] = useState(null);
+    const [primaryCountry, setPrimaryCountry] = useState('US');
     const [primaryProvinces, setPrimaryProvinces] = useState([]);
-    const [primaryProvince, setPrimaryProvince] = useState(null);
+    const [primaryProvince, setPrimaryProvince] = useState('All');
     const [primaryMetric, setPrimaryMetric] = useState('confirmed');
     const [primaryChartData, setPrimaryChartData] = useState([]);
     const [primaryTotals, setPrimaryTotals] = useState('daily');
 
+    const curatePrimaryChartData = props => {
+        const args = {
+            sample: props.data || covidData,
+            country: props.country || primaryCountry,
+            province: props.province || primaryProvince,
+            metric: props.metric || primaryMetric,
+            totals: props.totals || primaryTotals
+        };
+        let data = args.sample.reduce((agg, item) => {
+            if (item.country !== args.country) return agg;
+            if (args.province !== 'All' && item.province !== args.province) return agg;
+            if (typeof item.date !== 'string') item.date = `${item.date.getMonth() + 1}/${item.date.getDate()}`;
+            const index = agg.findIndex(a => a.date === item.date);
+            if (index >= 0) return [...agg.slice(0, index), { ...agg[index], [args.metric]: agg[index][args.metric] + parseInt(item[args.metric]) }];
+            const obj = { date: item.date, country: item.country, [args.metric]: parseInt(item[args.metric]) || 0 };
+            if (args.province) obj.province = item.province;
+            return [...agg, obj];
+        }, []);
+        if (args.totals === 'cumulative') return setPrimaryChartData(data);
+        data = data.map((item, i, arr) => (i === 0 ? item : { ...item, [args.metric]: (item[args.metric] - arr[i - 1][args.metric]) || 0 }));
+        setPrimaryChartData(data);
+    };
+
     useEffect(() => {
         const fetchData = async () => {
+            const allData = [];
             for (let i = 1; i <= Math.floor(diff); i += 1) {
                 const date = new Date(startTime + i * msInDay);
                 const res = await fetch(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/${getDateString(date)}.csv`);
@@ -52,30 +76,20 @@ const SecondPage = () => {
                     recovered: item['Recovered'],
                     deaths: item['Deaths'],
                 }));
+                allData.push(...arr);
                 setCovidData(agg => ([...agg, ...arr]));
                 setLoading(Math.floor(i / Math.floor(diff) * 100));
+                if (i === Math.floor(diff)) {
+                    const provinces = allData.reduce((agg, item) => (item.country === primaryCountry && !agg.includes(item.province) && item.province !== '' ? [...agg, item.province] : agg), []);
+                    setPrimaryProvinces(provinces);
+                    curatePrimaryChartData({ data: allData });
+                };
             }
         }
         fetchData();
     }, []);
 
-    useEffect(() => setChartWidth(window.innerWidth > 940 ? 900 : window.innerWidth - 40))
-
-    const curatePrimaryChartData = args => {
-        let data = covidData.reduce((agg, item) => {
-            if (item.country !== args.country) return agg;
-            if (args.province && item.province !== args.province) return agg;
-            if (typeof item.date !== 'string') item.date = `${item.date.getMonth() + 1}/${item.date.getDate()}`;
-            const index = agg.findIndex(a => a.date === item.date);
-            if (index >= 0) return [...agg.slice(0, index), { ...agg[index], [args.metric]: agg[index][args.metric] + parseInt(item[args.metric]) }];
-            const obj = { date: item.date, country: item.country, [args.metric]: parseInt(item[args.metric]) || 0 };
-            if (args.province) obj.province = item.province;
-            return [...agg, obj];
-        }, []);
-        if (args.totals === 'cumulative') return setPrimaryChartData(data);
-        data = data.map((item, i, arr) => (i === 0 ? item : { ...item, [args.metric]: (item[args.metric] - arr[i - 1][args.metric]) || 0 }));
-        setPrimaryChartData(data);
-    };
+    useEffect(() => setChartWidth(window.innerWidth > 940 ? 900 : window.innerWidth - 40));
 
     const countries = covidData
         .reduce((agg, item) => (agg.includes(item.country) ? agg : [...agg, item.country]), [])
@@ -86,7 +100,8 @@ const SecondPage = () => {
         setPrimaryCountry(e.target.value);
         const provinces = covidData.reduce((agg, item) => (item.country === e.target.value && !agg.includes(item.province) && item.province !== '' ? [...agg, item.province] : agg), []);
         setPrimaryProvinces(provinces);
-        curatePrimaryChartData({ country: e.target.value, province: primaryProvince, metric: primaryMetric, totals: primaryTotals });
+        setPrimaryProvince('All');
+        curatePrimaryChartData({ country: e.target.value, province: 'All' });
     };
 
     const primaryProvinceOptions = primaryProvinces
@@ -94,19 +109,18 @@ const SecondPage = () => {
         .map(province => <option key={province}>{province}</option>);
 
     const _handlePrimaryProvinceChange = e => {
-        const val = e.target.value === 'All' ? null : e.target.value;
-        setPrimaryProvince(val);
-        curatePrimaryChartData({ country: primaryCountry, province: val, metric: primaryMetric, totals: primaryTotals });
+        setPrimaryProvince(e.target.value);
+        curatePrimaryChartData({ province: e.target.value });
     };
 
     const _handlePrimaryMetricChange = e => {
         setPrimaryMetric(e.target.value);
-        curatePrimaryChartData({ country: primaryCountry, province: primaryProvince, metric: e.target.value, totals: primaryTotals });
+        curatePrimaryChartData({ metric: e.target.value });
     };
 
     const _handlePrimaryTotalsChange = e => {
         setPrimaryTotals(e.target.value);
-        curatePrimaryChartData({ country: primaryCountry, province: primaryProvince, metric: primaryMetric, totals: e.target.value });
+        curatePrimaryChartData({ totals: e.target.value });
     };
 
     return (
@@ -115,16 +129,16 @@ const SecondPage = () => {
             <h1>Coronavirus Data Playground</h1>
             {loading < 100 && <p>Data loading {loading}% complete</p> || (
                 <div>
-                    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                        <div style={{ display: 'flex', flexDirection: 'column', flex: 1, padding: '10px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: chartWidth === 900 ? 'no-wrap' : 'wrap', justifyContent: 'center', marginBottom: 20 }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 450, width: chartWidth === 900 ? (primaryProvinces.length && '25%' || '33%') : '100%', padding: '10px' }}>
                             <label>Region:</label>
-                            <select onChange={_handlePrimaryCountryChange}>
+                            <select onChange={_handlePrimaryCountryChange} defaultValue={primaryCountry}>
                                 <option>Please select a country</option>
                                 {countries}
                             </select>
                         </div>
                         {primaryProvinces.length > 0 && (
-                            <div style={{ display: 'flex', flexDirection: 'column', flex: 1, padding: '10px' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 450, width: chartWidth === 900 ? (primaryProvinces.length && '25%' || '33%') : '100%', padding: '10px' }}>
                                 <label>Subregion:</label>
                                 <select onChange={_handlePrimaryProvinceChange}>
                                     <option>All</option>
@@ -132,7 +146,7 @@ const SecondPage = () => {
                                 </select>
                             </div>
                         )}
-                        <div style={{ display: 'flex', flexDirection: 'column', flex: 1, padding: '10px' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 450, width: chartWidth === 900 ? (primaryProvinces.length && '25%' || '33%') : '100%', padding: '10px' }}>
                             <label>Metric:</label>
                             <select onChange={_handlePrimaryMetricChange}>
                                 <option value="confirmed">Confirmed</option>
@@ -140,7 +154,7 @@ const SecondPage = () => {
                                 <option value="deaths">Deaths</option>
                             </select>
                         </div>
-                        <div style={{ display: 'flex', flexDirection: 'column', flex: 1, padding: '10px' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 450, width: chartWidth === 900 ? (primaryProvinces.length && '25%' || '33%') : '100%', padding: '10px' }}>
                             <label>Totals:</label>
                             <select onChange={_handlePrimaryTotalsChange}>
                                 <option value="daily">Daily</option>
